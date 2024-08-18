4ca5dd7dbc5d84530447622559662e7944f651dd
ProjectAkhirBelajarMembuatAplikasiAndroidPemulaDicoding <khairililmi2468@gmail.com>
2024-08-19 05:36:50 +0700

    second commit

--- frontend/app/Views/partials/header.php
+++ frontend/app/Views/partials/header.php
@@ -9,3 +9,5 @@
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
